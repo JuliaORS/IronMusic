110c3c36ee83716d5c2bbd5a0917d03498bb5c81
JuliaORS <julia.olle.roset@gmail.com>
2024-04-18 22:14:09 +0200

    playlist model

---
+++ src/main/java/com/ironhack/model/Playlist.java
@@ -0,0 +1,29 @@
+package com.ironhack.model;
+
+import com.ironhack.demosecurityjwt.security.models.Artist;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Playlist {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "playlist_audio", //join table name
+            joinColumns = @JoinColumn(name = "playlist_id"), // ref column this entity
+            inverseJoinColumns = @JoinColumn(name = "audio_id") // audio ref column
+    )
+    private List<Audio> audios;
+}
